ff05897e3e39452212d03a460adbe3eb0361ff7e
Onosans <10ryou20ta5819@gmail.com>
2024-11-23 11:06:20 +0900

    [add]complete

--- test.c
+++ test.c
@@ -1,9 +1,28 @@
-#include <stdio.h>
+#include "stdio.h"
+#define N 6
+int judge(int num);
 
 int main(void){
-	if(i == j){
-		f = g + h;
-	} else {
-		f = g- h;
-	}
+  int even_sum, odd_sum, i, z;
+  even_sum = 0;
+  odd_sum = 0;
+
+  for(i=0; i < N; i++){
+    z = judge(i);
+    if(z == 0){
+      even_sum = even_sum + i;
+    } else {
+      odd_sum = odd_sum + i;
+    }
+  }
+
+  return 0;
+}
+
+int judge(int num){
+  int deteo, x, y;
+  x = num / 2;
+  y = x * 2;
+  deteo = num - y;
+  return(deteo);
 }
